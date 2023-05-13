90ce619ae58c968b8371c81843cd6a7d82777966
alantmatos <mr.alanmatos@hotmail.com>
2023-05-13 15:35:55 -0400

    sign up page

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 
 
 refactoring capstone Back Home app
+
+for v0.00.0 see [https://github.com/alantmatos/backhome-app]
--- src/Components/SignUp/Signup.js
+++ src/Components/SignUp/Signup.js
@@ -1,8 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './index.css';
+
 
 const Signup = () => {
+
+  let [modalDisplay, setModalDisplay] = useState(false);
+
+
+
   return (
-    <div>Signup</div>
+    <div className='signup-container'>
+
+
+      <div className='left'>
+        <div className='signup-tittle'>
+          <h3>Yay, lets create an account!</h3> <br></br>
+
+          <h5>
+            You made the right choice. <br>
+            </br>Our goal is too make your pet safer, and we truly hope you will never have your pet lost,
+            but in case you do, be sure we will always be working on ways to improve its chances to get back to you.
+          </h5>
+        </div>
+
+      </div>
+
+      {!modalDisplay ?
+        <div className='right'>
+          <form>
+            <div>
+              <label>Name:</label><br></br>
+              <input type='text'></input>
+            </div>
+
+            <div>
+              <label>Email:</label><br></br>
+              <input type='text'></input>
+            </div>
+
+            <div>
+              <label>Password:</label><br></br>
+              <input type='text'></input>
+            </div>
+
+            <div className='btn-container'> <br></br>
+              <button className='signup-btn' onClick={() => setModalDisplay(!modalDisplay)}>Sign Up</button>
+            </div>
+          </form>
+        </div>
+        : null}
+
+
+
+      {modalDisplay ?
+        <div className='modal-card'>
+          <form>
+            <div>
+              <label>Please enter the PIN sent on your email:</label>
+              <input type='number'></input>
+            </div>
+
+            <div className='btn-container'>
+              <button type=''> Sign Up</button>
+            </div>
+          </form>
+        </div>
+        : null}
+    </div>
   )
 }
 
